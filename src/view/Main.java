7c642accbcab22ae54e62cbbfed42fe2b7727c9a
thienduyen12930 <thienduyen1209@gmail.com>
2023-09-19 13:20:38 +0700

    Manage student

---
+++ src/view/Main.java
@@ -0,0 +1,8 @@
+import controller.ManageStudents;
+
+public class Main {
+        public static void main(String[] args) {
+        ManageStudents manageStudents = new ManageStudents();
+        manageStudents.run();
+    }
+}
